d129d73f843fc9ae16109d663942a05ebfad114b
AnyaWMa <76414800+AnyaWMa@users.noreply.github.com>
2024-11-22 09:33:26 -0800

    update 00funs.R

--- analysis/00funs.R
+++ analysis/00funs.R
@@ -17,13 +17,46 @@ cdm.pr<-function(resp,qm0,modeltype="DINA") {
     library(GDINA)
     m <- GDINA(resp,qm0,model=modeltype)
     map <- personparm(m, what = "EAP")[,1:ncol(qm0)]
-    gs<-do.call("rbind",coef(m))
+    gs<-coef(m,what='gs')
     p<-list()
     for (i in 1:nrow(qm0)) {
         ii<-which(qm0[i,]==1)
         z<-map[,ii,drop=FALSE]
         rm<-rowMeans(z)
-        p[[i]]<-ifelse(rm==1,gs[i,2],gs[i,1])
+        p[[i]]<-ifelse(rm==1,1-gs[i,2],gs[i,1])
     }
     p.cdm<-do.call("cbind",p)
+}
+
+oos.compare<-function(resp,qm0,nfolds) {
+    id<-1:nrow(resp)
+    item<-names(resp)
+    L<-list()
+    for (i in 1:ncol(resp)) L[[i]]<-data.frame(id=id,item=names(resp)[i],resp=resp[,i])
+    df<-do.call("rbind",L)
+    df$gr<-sample(1:nfolds,nrow(df),replace=TRUE)
+    
+    om<-numeric()
+    for (gr in unique(df$gr)) {
+        oos<-df[df$gr==gr,]
+        ins<-df[df$gr!=gr,]
+        x<-irw::long2resp(ins)
+        id<-x$id
+        x<-x[,names(resp)]
+        ##
+        p.cdm<-cdm.pr(x,qm0)
+        L<-list()
+        for (i in 1:ncol(resp)) L[[i]]<-data.frame(id=id,item=names(resp)[i],p.cdm=p.cdm[,i])
+        p<-do.call("rbind",L)
+        oos<-merge(oos,p)
+        ##
+        p.irt<-irt.pr(x)
+        L<-list()
+        for (i in 1:ncol(resp)) L[[i]]<-data.frame(id=id,item=names(resp)[i],p.irt=p.irt[,i])
+        p<-do.call("rbind",L)
+        oos<-merge(oos,p)
+        ##
+        om[gr]<-imv::imv.binary(oos$resp,oos$p.irt,oos$p.cdm)
+    }
+    mean(om)
 }
