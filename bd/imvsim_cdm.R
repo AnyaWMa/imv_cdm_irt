0394d9d7be1006f0cb930cf4a730871f30b40005
Ben Domingue <ben.domingue@gmail.com>
2024-11-23 08:22:16 -0800

    updating simulation code

--- bd/imvsim_cdm.R
+++ bd/imvsim_cdm.R
@@ -1,52 +1,91 @@
+##idea: you are taking within-item correlations below. might be better to take items across all correlations
+
+
 source("00funs.R")
 
 
 ################################3
 ##simulate data with naughty cdm
-cdm.sim<-function(N) {
+cdm.sim<-function(a,N=5000) {
     ##skills
-    sk<-rbinom(5*N,1,.5)
-    sk<-matrix(sk,nrow=N,ncol=5)
+                                        #sk<-rbinom(5*N,1,.5)
+                                        #sk<-matrix(sk,nrow=N,ncol=5)
+    th<-rnorm(N)
+    sk<-runif(5)
+    p<-a*(outer(th,sk-mean(sk),'-'))
+    p<-apply(p,2,function(x) 1/(1+exp(-x)))
+    sk<-apply(p,2,function(x) rbinom(nrow(p),1,p))
+          
     ##qmatrix
     #qm<-diag(5)
-    qm<-matrix(rbinom(10*5,1,.5),10,5)
-    qm0<-rbind(qm,qm,qm)
+    S<-TRUE
+    while (S) {
+        qm<-matrix(rbinom(5*30,1,.5),30,5)
+        S<-any(c(rowSums(qm),colSums(qm))==0)
+    }            
+    qm0<-qm
     ##response probabilities 
     pL<-respL<-list()
-    g<-c(.1,.2,.3)
-    for (j in 1:3) {
-        p<-resp<-list()
-        for (i in 1:nrow(qm)) {
-            ii<-which(qm[i,]==1)
-            z<-sk[,ii,drop=FALSE]
-            rm<-rowMeans(z)
-            p.tmp<-ifelse(rm==1,1-g[j],g[j])
-            resp[[i]]<-rbinom(N,1,p.tmp)
-            p[[i]]<-p.tmp
-        }
-        respL[[j]]<-do.call("cbind",resp)
-        pL[[j]]<-do.call("cbind",p)
+    g<-runif(nrow(qm0),min=0,max=.35)
+    s<-runif(nrow(qm0),min=0,max=.35)
+    for (i in 1:nrow(qm0)) {
+        ii<-which(qm[i,]==1)
+        z<-sk[,ii,drop=FALSE]
+        rm<-rowMeans(z)
+        p.tmp<-ifelse(rm==1,1-s[i],g[i])
+        respL[[i]]<-rbinom(N,1,p.tmp)
+        pL[[i]]<-p.tmp
     }
     resp<-do.call("cbind",respL)
     p.true<-do.call("cbind",pL)
     resp<-as.data.frame(resp)
     names(resp)<-paste("i",1:ncol(resp),sep='')
     
-    ## p.irt<-irt.pr(resp)
-    ## p.cdm<-cdm.pr(resp,qm0)
+    p.irt<-irt.pr(resp)
+    p.cdm<-cdm.pr(resp,qm0)
     
     ## ##using true values
-    ## coors<-list()
-    ## for (i in 1:ncol(resp)) coors[[i]]<-cor(cbind(resp[,i],p.true[,i],p.irt[,i],p.cdm[,i]))
-    ## ##note that third row << fourth row
-    ## sapply(coors,function(x) x[1,])
+    coors<-list()
+    for (i in 1:ncol(resp)) coors[[i]]<-cor(cbind(resp[,i],p.true[,i],p.irt[,i],p.cdm[,i]))
+    ##note that third row << fourth row
+    s<-sapply(coors,function(x) x[1,])
+    true<-rowMeans(s)[3:4]
     
     ##cv imv values
-    oos.compare(resp,qm0,nfolds=4)
+    om<-oos.compare(resp,qm0,nfolds=5)
+    list(t=true,om=om)
 }
 
-N<-runif(5,3,4.5)
-N<-round(10^N)
-om<-sapply(N,cdm.sim)
+## N<-runif(50,3,4.5)
+## N<-round(10^N)
+## om<-sapply(N,cdm.sim)
 
-plot(N,om,pch=19); abline(h=0)
+a<-sort(runif(100,min=0,max=3))
+library(parallel)
+out<-mclapply(a,cdm.sim,mc.cores=4)
+
+
+#pdf("/home/bdomingu/Dropbox/Apps/Overleaf/CDM_predictions/scenario1.pdf",width=4,height=2.5)
+par(mgp=c(2,1,0),mfrow=c(1,2))
+
+####
+tr<-lapply(out,function(x) x$t)
+
+z<-do.call("rbind",tr)
+plot(a,z[,1],type='l',col='black',ylim=0:1)
+lines(a,z[,2],type='l',col='red')
+
+
+#####
+om<-lapply(out,function(x) x$om)
+df<-data.frame(a=a,om=unlist(om))
+
+plot(df$a,df$om,pch=19,col='lightgray',cex=.5,ylab="IMV",xlab='a')
+abline(h=0)
+
+m<-loess(om~a,df)
+pr<-predict(m,df$a,se=TRUE)
+lines(df$a,pr$fit,col='red',lwd=TRUE)
+cc<-col2rgb("red")
+polygon(c(df$a,rev(df$a)),c(pr$fit+1.96*pr$se.fit,rev(pr$fit-1.96*pr$se.fit)),border=NA,col=rgb(cc[1],cc[2],cc[3],max=255,alpha=30))
+#dev.off()
--- bd/sim_md.R
+++ bd/sim_md.R
@@ -1,7 +1,7 @@
 ##equal share of items load on two dimensions
 source("00funs.R")
 simfun<-function(r) {
-    N<-1000
+    N<-5000
     library(MASS)
     ##theta
     th<-mvrnorm(N,mu=rep(0,2),Sigma=matrix(c(1,r,r,1),2,2))
@@ -36,31 +36,27 @@ simfun<-function(r) {
     ##within-item MD
     S<-TRUE
     while (S) {
-        qm<-matrix(rbinom(14*6,1,.5),14,6)
+        qm<-matrix(rbinom(30*6,1,.5),30,6)
         S<-any(rowMeans(qm)==0)
     }        
-    qm0<-rbind(qm,qm,qm)
-    
+    qm0<-qm
+
     ##response probabilities for with g=s=0.1
     pL<-respL<-list()
-    for (j in 1:3) {
-        p<-resp<-list()
-        g<-s<-0.1*j
-        for (i in 1:nrow(qm)) {
-            ii<-which(qm[i,]==1)
-            z<-sk[,ii,drop=FALSE]
-            rm<-rowMeans(z)
-            p.tmp<-ifelse(rm==1,1-s,g)
-            resp[[i]]<-rbinom(N,1,p.tmp)
-            p[[i]]<-p.tmp
-        }
-        respL[[j]]<-do.call("cbind",resp)
-        pL[[j]]<-do.call("cbind",p)
+    g<-runif(nrow(qm0),min=0,max=.35)
+    s<-runif(nrow(qm0),min=0,max=.35)
+    for (i in 1:nrow(qm0)) {
+        ii<-which(qm[i,]==1)
+        z<-sk[,ii,drop=FALSE]
+        rm<-rowMeans(z)
+        p.tmp<-ifelse(rm==1,1-s[i],g[i])
+        respL[[i]]<-rbinom(N,1,p.tmp)
+        pL[[i]]<-p.tmp
     }
     resp<-do.call("cbind",respL)
     p.true<-do.call("cbind",pL)
     resp<-as.data.frame(resp)
-    names(resp)<-paste("i",1:ncol(resp))
+    names(resp)<-paste("i",1:ncol(resp),sep='')
     
     p.irt<-irt.pr(resp)
     p.cdm<-cdm.pr(resp,qm0)
@@ -68,16 +64,26 @@ simfun<-function(r) {
     L<-list(as.matrix(resp),p.true,p.irt,p.cdm)
     L<-lapply(L,as.numeric)
     z<-do.call("cbind",L)
-    cor(z)
+    true<-cor(z)[2,3:4]
+    
+    ##cv imv values
+    om<-oos.compare(resp,qm0,nfolds=5)
+    list(t=true,om=om)
+
 }
 
-rs<-sort(runif(200,-1,1))
+rs<-sort(runif(250,-1,1))
 library(parallel)
 L<-mclapply(rs,simfun,mc.cores=4)
 
-irt<-sapply(L,function(x) x[2,3])
-cdm<-sapply(L,function(x) x[2,4])
-par(mgp=c(2,1,0))
+tr<-lapply(L,function(x) x$t)
+tr<-do.call("rbind",tr)
+om<-sapply(L,function(x) x$om)
+
+par(mgp=c(2,1,0),mfrow=c(1,2))
+##
+irt<-tr[,1]
+cdm<-tr[,2]
 plot(NULL,xlim=c(-1,1),ylim=0:1,xlab='correlation between dimensions',ylab='correlation between estimates and true probabilities')
 pf<-function(x,y,...) {
     m<-loess(y~x)
@@ -86,4 +92,7 @@ pf<-function(x,y,...) {
 lines(pf(rs,irt))
 lines(pf(rs,cdm,col='red'))
 legend("bottomright",bty='n',fill=c("black","red"),c("irt","cdm"))
+##
+plot(NULL,xlim=c(-1,1),ylim=c(-.05,.15),xlab='correlation between dimensions',ylab='IMV')
+lines(pf(rs,om))
 
