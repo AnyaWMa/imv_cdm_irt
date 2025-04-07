fca7175816cb9155df5e9d32ae4a9c88a1953e11
AnyaWMa <76414800+AnyaWMa@users.noreply.github.com>
2025-04-06 23:12:40 -0700

    update simulations

--- bd/scenario1.R
+++ bd/scenario1.R
@@ -1,6 +1,7 @@
 remotes::install_github("hansorlee/irwpkg")
 source("00funs.R") ##https://github.com/AnyaWMa/IRW-Qmatrix/blob/main/bd/00funs.R
 library(GDINA)
+library(parallel)
 ################################3
 ##simulate data with naughty cdm
 cdm.sim<-function(a,N=500,sk.offset=0,nsk=6,bound=NULL, modeltype = "DINA",estmethod="MAP") {
@@ -11,7 +12,7 @@ cdm.sim<-function(a,N=500,sk.offset=0,nsk=6,bound=NULL, modeltype = "DINA",estme
     p<-p+sk.offset ##controlling prevalence of skills
     p<-apply(p,2,function(x) 1/(1+exp(-(x))))
     sk<-apply(p,2,function(x) rbinom(nrow(p),1,p))
-    ##qmatrix
+    ## qmatrix
     S<-TRUE
     
     n_valid <- 44     # desired number of valid rows
@@ -101,54 +102,189 @@ cdm.sim<-function(a,N=500,sk.offset=0,nsk=6,bound=NULL, modeltype = "DINA",estme
     ##
     om
 }
-a<-sort(runif(n = 100,min=0,max=3))
-library(parallel)
 
-out2<-list()
-for (i in c(.1, 0.2, 0.3)) out2[[as.character(i)]]<-mclapply(a,cdm.sim,mc.cores=10,sk.offset=1.5,bound=i,  modeltype = "GDINA",estmethod = "mp")
-#out2[["a"]] <- a
+simulate_scenario_1 <- function(modeltype = "GDINA", estmethod = "mp") {
+  # Generate sorted vector a
+  a <- sort(runif(n = 100, min = 0, max = 3))
+  
+  # Initialize output list
+  out2 <- list()
+  
+  # Loop over bounds and apply cdm.sim in parallel
+  for (i in c(0.1, 0.2, 0.3)) {
+    out2[[as.character(i)]] <- mclapply(
+      a,
+      cdm.sim,
+      mc.cores = 10,
+      sk.offset = 1.5,
+      bound = i,
+      modeltype = modeltype,
+      estmethod = estmethod
+    )
+  }
+  
+  # Return both a and out2
+  return(list(a = a, out2 = out2))
+}
 
-out2dina<-list()
-for (i in c(.1)) out2dina[[as.character(i)]]<-mclapply(a,cdm.sim,mc.cores=10,sk.offset=1.5,bound=i,  modeltype = "DINA",estmethod = "mp")
+## Simulate GDINA
+result_gdina_1 <- simulate_scenario_1(modeltype = "GDINA", estmethod = "mp")
+save(result_gdina_1 , file = "../simulation_data/scenario1_out_gdina_offset15_ps035_mp.RData")
+
+## Simulate GDINA
+result_dina_1 <- simulate_scenario_1(modeltype = "DINA", estmethod = "mp")
+save(result_dina_1 , file = "../simulation_data/scenario1_out_dina_offset15_ps035_mp.RData")
 #save(out2 , file = "../simulation_data/out_dina_offset15_ps035.RData")
 
-#pdf("/home/bdomingu/Dropbox/Apps/Overleaf/CDM_predictions/scenario1.pdf",width=6,height=3)
-par(mgp=c(2,1,0),mfrow=c(3,2),mar=c(3,3,1,1),oma=rep(.5,4))
-####
-for (i in 1:length(out2)) {
-    plot(NULL,xlim=c(0,3),ylim=c(0,.6),xlab='a',ylab='RMSE(oos resp,est)')
-    pf<-function(x,y,...) {
-        m<-loess(y~x)
-        lines(x,predict(m),...,lwd=2)
+simulate_scenario_2 <- function() {
+  # Generate sorted vector a
+  a <- sort(runif(n = 100, min = 0, max = 3))
+  
+  # Initialize output list
+  out2 <- list()
+  
+  # Loop over model types and estimation methods
+  for (i in c("DINA", "GDINA")) {
+    print(i)
+    for (j in c("mp", "MAP")) {
+      print(j)
+      key <- paste(i, j, sep = "_")
+      out2[[key]] <- mclapply(
+        a,
+        cdm.sim,
+        mc.cores = 10,
+        sk.offset = 1.5,
+        bound = 0.1,
+        modeltype = i,
+        estmethod = j
+      )
     }
-    f<-function(out,...) {
-        z<-do.call("rbind",out)
-        irt<-z[,4]
-        cdm<-z[,5]
-        irt.p<-z[,6]
-        cdm.p<-z[,7]
-        pf(a,irt,col='blue',...)
-        pf(a,cdm,col='red',...)
-        pf(a,irt.p,col='blue',...,lty=2)
-        pf(a,cdm.p,col='red',...,lty=2)
-    }
-                                        #f(out1)
-    f(out2[[i]])
-    legend("topright",bty='n',lty=c(1,1,2,2),col=c("blue","red","blue","red"),cex=.7,
-           c("(resp,IRT)","(resp,CDM)","(True,IRT)","(True,CDM)"))
-#####
-    plot(NULL,xlim=c(0,3),ylab="IMV",xlab='a',ylim=c(-.1,.1))
-    f<-function(out,...) {
-        om<-do.call("rbind",out)
-        abline(h=0)
-        pf(a,om[,1],...)
-        pf(a,om[,2],col='blue',...,lty=2)
-        pf(a,om[,3],col='red',...,lty=2)
-    }
-    f(out2[[i]])
-    legend("topright",bty='n',lty=c(1,2,2),col=c("black","blue","red"),cex=.7,
-           c("(IRT,CDM)","(IRT,True)","(CDM,True)")
-           )
+  }
+  
+  # Return both a and out2
+  return(list(a = a, out2 = out2))
 }
+
+result_compare_estimator <- simulate_scenario_2()
+save(result_compare_estimator , file = "../simulation_data/scenario1_result_compare_estimator.RData")
+#pdf("/home/bdomingu/Dropbox/Apps/Overleaf/CDM_predictions/scenario1.pdf",width=6,height=3)
+
+plot_rmse_imv_panels <- function(a, out2) {
+  par(mgp = c(2, 1, 0), mfrow = c(3, 2), mar = c(3, 3, 2, 1), oma = rep(0.5, 4))
+  
+  panel_titles <- c("RMSE (High item quality)", "IMV (High item quality)", 
+                    "RMSE (Medium item quality)", "IMV (Medium item quality)", 
+                    "RMSE (Low item quality)", "IMV (Low item quality)")
+  
+  pf <- function(x, y, ...) {
+    m <- loess(y ~ x)
+    lines(x, predict(m), ..., lwd = 2)
+  }
+  
+  for (i in seq_along(out2)) {
+    
+    ## RMSE Plot
+    plot(NULL, xlim = c(0, 3), ylim = c(0, 0.6), 
+         xlab = 'a', ylab = 'RMSE(oos resp, est)')
+    title(panel_titles[2 * i - 1], line = 0.5, cex.main = 1)
+    mtext(paste0("(", letters[2 * i - 1], ")"), side = 3, adj = 0, cex = 1)
+    
+    f_rmse <- function(out, ...) {
+      z <- do.call("rbind", out)
+      irt <- z[, 4]
+      cdm <- z[, 5]
+      irt.p <- z[, 6]
+      cdm.p <- z[, 7]
+      pf(a, irt, col = 'blue', ...)
+      pf(a, cdm, col = 'red', ...)
+      pf(a, irt.p, col = 'blue', lty = 2, ...)
+      pf(a, cdm.p, col = 'red', lty = 2, ...)
+    }
+    
+    f_rmse(out2[[i]])
+    
+    legend("topright", bty = 'n', lty = c(1, 1, 2, 2),
+           col = c("blue", "red", "blue", "red"), cex = 1,
+           legend = c("(resp,IRT)", "(resp,CDM)", "(True,IRT)", "(True,CDM)"))
+    
+    ## IMV Plot
+    plot(NULL, xlim = c(0, 3), ylim = c(-0.05, 0.2),
+         xlab = 'a', ylab = 'IMV')
+    title(panel_titles[2 * i], line = 0.5, cex.main = 1)
+    mtext(paste0("(", letters[2 * i], ")"), side = 3, adj = 0, cex = 1)
+    
+    f_imv <- function(out, ...) {
+      om <- do.call("rbind", out)
+      abline(h = 0)
+      pf(a, om[, 1], ...)
+      pf(a, om[, 2], col = 'blue', lty = 2, ...)
+      pf(a, om[, 3], col = 'red', lty = 2, ...)
+    }
+    
+    f_imv(out2[[i]])
+    
+    legend("topright", bty = 'n', lty = c(1, 2, 2),
+           col = c("black", "blue", "red"), cex = 1,
+           legend = c("(IRT,CDM)", "(IRT,True)", "(CDM,True)"))
+  }
+}
+pdf("../plots/simulation1_gdina_mp.pdf", width = 6, height = 8)
+plot_rmse_imv_panels(result_gdina_1$a, result_gdina_1$out2)
+if (!is.null(file)) dev.off()
+
+pdf("../plots/simulation1_dina_mp.pdf", width = 6, height = 8)
+plot_rmse_imv_panels(result_dina_1$a, result_dina_1$out2)
+if (!is.null(file)) dev.off()
+
+plot_imv_only_panels <- function(a, out2) {
+  # Open new graphics window to reset layout (works in RStudio)
+  if (dev.cur() != 1) dev.new(width = 10, height = 8)
+  
+  par(mfrow = c(2, 2), mar = c(3, 3, 2, 1), oma = rep(0.5, 4), mgp = c(2, 1, 0))
+  
+  panel_titles <- c(
+    "DINA with mp", 
+    "DINA with MAP", 
+    "G-DINA with mp", 
+    "G-DINA with MAP"
+  )
+  
+  keys <- c("DINA_mp", "DINA_MAP", "GDINA_mp", "GDINA_MAP")
+  
+  pf <- function(x, y, ...) {
+    m <- loess(y ~ x)
+    lines(x, predict(m), ..., lwd = 2)
+  }
+  
+  for (i in seq_along(keys)) {
+    plot(NULL, xlim = c(0, 3), ylim = c(-0.05, 0.2),
+         xlab = 'a', ylab = 'IMV', main = panel_titles[i], cex.main = 1)
+    mtext(paste0("(", letters[i], ")"), side = 3, adj = 0, cex = 1)
+    abline(h = 0)
+    
+    out <- out2[[keys[i]]]
+    if (!is.null(out)) {
+      om <- do.call("rbind", out)
+      pf(a, om[, 1], col = 'black')
+      pf(a, om[, 2], col = 'blue', lty = 2)
+      pf(a, om[, 3], col = 'red', lty = 2)
+      
+      legend("topright", bty = 'n', lty = c(1, 2, 2),
+             col = c("black", "blue", "red"), cex = 0.9,
+             legend = c("(IRT,CDM)", "(IRT,True)", "(CDM,True)"))
+    } else {
+      warning(paste("No data for", keys[i]))
+    }
+  }
+}
+
+pdf("../plots/simulation1_dina_gdina_mp_MAP.pdf", width = 6, height = 6)
+plot_imv_only_panels(result_compare_estimator$a, result_compare_estimator$out2)
+if (!is.null(file)) dev.off()
+
+
+##legend("topright",bty='n',lwd=1,lty=c(1,2),title=expression(delta),legend=c(0,1.5))
+##dev.off()
+
 ##legend("topright",bty='n',lwd=1,lty=c(1,2),title=expression(delta),legend=c(0,1.5))
 ##dev.off()
--- bd/scenario3.R
+++ bd/scenario3.R
@@ -1,7 +1,8 @@
 ##equal share of items load on two dimensions
 remotes::install_github("hansorlee/irwpkg")
 source("00funs.R")
-simfun<-function(r,N=500,bound=NULL) {
+library(parallel)
+simfun<-function(r,N=500,bound=NULL, modeltype = "DINA",estmethod="MAP") {
     library(MASS)
     ##theta
     th<-mvrnorm(N,mu=rep(0,2),Sigma=matrix(c(1,r,r,1),2,2))
@@ -53,59 +54,100 @@ simfun<-function(r,N=500,bound=NULL) {
     p.irt<-irt.pr(resp)
     p.cdm<-cdm.pr.marg(resp,qm)
     ##cv imv values
-    om<-oos.compare.newresp(resp,qm,truep=p.true)
+    om<-oos.compare.newresp(resp,qm,truep=p.true, modeltype = modeltype, estmethod = estmethod)
     ##
     om
 }
 
-rs<-sort(runif(100,-1,1))
-library(parallel)
-#L<-mclapply(rs,simfun,mc.cores=10)
-
-L<-list()
-for (bound in c(.1,.2,.3)) L[[as.character(bound)]]<-mclapply(rs,simfun,mc.cores=10,bound=bound)
-
-#L[["rs"]] <- rs
-#save(L , file = "../simulation_data/scenario2_dina.RData")
-
-pdf("../plots/scenario2_update.pdf",width=6,height=3)
-#par(mgp=c(2,1,0),mfrow=c(1,2),mar=c(3,3,1,1),oma=rep(.5,4))
-par(mgp=c(2,1,0),mfrow=c(3,2),mar=c(3,3,1,1),oma=rep(.5,4))
-for (i in 1:length(L)) {
-    plot(NULL,xlim=c(-1,1),ylim=c(0,.6),xlab=expression(rho),ylab='RMSE(true,est)')
-    pf<-function(x,y,...) {
-        m<-loess(y~x)
-        lines(x,predict(m),...,lwd=2)
-    }
-    f<-function(out,...) {
-        z<-do.call("rbind",out)
-        irt<-z[,4]
-        cdm<-z[,5]
-        irt.p<-z[,6]
-        cdm.p<-z[,7]
-        pf(rs,irt,col='blue',...)
-        pf(rs,cdm,col='red',...)
-        pf(rs,irt.p,col='blue',...,lty=2)
-        pf(rs,cdm.p,col='red',...,lty=2)
-    }
-                                        #f(out1)
-    f(L[[i]])
-    legend("topright",bty='n',lty=c(1,1,2,2),col=c("blue","red","blue","red"),cex=.7,ncol=2,
-           c("(resp,IRT)","(resp,CDM)","(True,IRT)","(True,CDM)"))
-#####
-    plot(NULL,xlim=c(c(-1,1)),ylab="IMV",xlab=expression(rho),ylim=c(0,.2))
-    f<-function(out,...) {
-        om<-do.call("rbind",out)
-        abline(h=0)
-        pf(rs,om[,1],...)
-        pf(rs,om[,2],col='blue',...,lty=2)
-        pf(rs,om[,3],col='red',...,lty=2)
-    }
-                                        #f(out1,lty=1)
-    f(L[[i]])
-    legend("topright",bty='n',lty=c(1,2,2),col=c("black","blue","red"),cex=.7,
-           c("(IRT,CDM)","(IRT,True)","(CDM,True)")
-           )
-    ##
+simulate_scenario_rs <- function(modeltype = "DINA", estmethod = "mp") {
+  # Generate sorted vector rs
+  rs <- sort(runif(n = 100, min = -1, max = 1))
+  
+  # Initialize output list
+  out <- list()
+  
+  # Loop over bounds and apply simfun in parallel
+  for (bound in c(0.1, 0.2, 0.3)) {
+    out[[as.character(bound)]] <- mclapply(
+      rs,
+      simfun,
+      mc.cores = 10,
+      bound = bound,
+      modeltype = modeltype,
+      estmethod = estmethod
+    )
+  }
+  
+  # Return both rs and output
+  return(list(rs = rs, out = out))
 }
-dev.off()
+
+# Example usage
+result_sim2_dina <- simulate_scenario_rs(modeltype = "DINA", estmethod = "mp")
+save(result_sim2_dina , file = "../simulation_data/scenario2_out_dina_mp.RData")
+
+plot_rmse_imv_rho_panels <- function(rs, L) {
+  par(mgp = c(2, 1, 0), mfrow = c(3, 2), mar = c(3, 3, 2, 1), oma = rep(0.5, 4))
+  
+  # Panel titles for each subplot
+  panel_titles <- c("RMSE (High item quality)", "IMV (High item quality)", 
+                    "RMSE (Medium item quality)", "IMV (Medium item quality)", 
+                    "RMSE (Low item quality)", "IMV (Low item quality)")
+  
+  pf <- function(x, y, ...) {
+    m <- loess(y ~ x)
+    lines(x, predict(m), ..., lwd = 2)
+  }
+  
+  for (i in seq_along(L)) {
+    ## RMSE Plot
+    plot(NULL, xlim = c(-1, 1), ylim = c(0, 0.6),
+         xlab = expression(rho), ylab = "RMSE(true, est)")
+    title(main = panel_titles[2 * i - 1], line = 0.5, cex.main = 1)
+    mtext(paste0("(", letters[2 * i - 1], ")"), side = 3, adj = 0, cex = 1)
+    
+    f_rmse <- function(out, ...) {
+      z <- do.call("rbind", out)
+      irt <- z[, 4]
+      cdm <- z[, 5]
+      irt.p <- z[, 6]
+      cdm.p <- z[, 7]
+      pf(rs, irt, col = 'blue', ...)
+      pf(rs, cdm, col = 'red', ...)
+      pf(rs, irt.p, col = 'blue', lty = 2, ...)
+      pf(rs, cdm.p, col = 'red', lty = 2, ...)
+    }
+    
+    f_rmse(L[[i]])
+    legend("topright", bty = 'n', lty = c(1, 1, 2, 2),
+           col = c("blue", "red", "blue", "red"), cex = 1,
+           legend = c("(resp,IRT)", "(resp,CDM)", "(True,IRT)", "(True,CDM)"))
+    
+    
+    ## IMV Plot
+    plot(NULL, xlim = c(-1, 1), ylim = c(0, 0.2),
+         xlab = expression(rho), ylab = "IMV")
+    title(main = panel_titles[2 * i], line = 0.5, cex.main = 1)
+    mtext(paste0("(", letters[2 * i], ")"), side = 3, adj = 0, cex = 1)
+    
+    f_imv <- function(out, ...) {
+      om <- do.call("rbind", out)
+      abline(h = 0)
+      pf(rs, om[, 1], ...)
+      pf(rs, om[, 2], col = 'blue', lty = 2, ...)
+      pf(rs, om[, 3], col = 'red', lty = 2, ...)
+    }
+    
+    f_imv(L[[i]])
+    
+    legend("topright", bty = 'n', lty = c(1, 2, 2), col = c("black", "blue", "red"), 
+           cex = 0.7,
+           legend = c("(IRT,CDM)", "(IRT,True)", "(CDM,True)"))
+  }
+}
+
+pdf("../plots/simulation2_dina_mp.pdf", width = 6, height = 8)
+plot_rmse_imv_rho_panels(result_sim2_dina$rs, result_sim2_dina$out)
+if (!is.null(file)) dev.off()
+
+
